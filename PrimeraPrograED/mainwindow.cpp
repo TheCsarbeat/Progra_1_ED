26d4c2dbf5b4fed5c7243bfc6365c405ca337a1d
TheCsarbeat <62681287+TheCsarbeat@users.noreply.github.com>
2021-10-19 16:36:35 -0600

    Deleted folder

--- PrimeraPrograED/mainwindow.cpp
+++ PrimeraPrograED/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->contentPanel->setCurrentIndex(0);
 }
 
 MainWindow::~MainWindow()
@@ -17,43 +18,7 @@ MainWindow::~MainWindow()
 void MainWindow::on_btnIniciar_clicked(){
 
 
-    RegistroAlmacen * registro = new RegistroAlmacen();
-    Carrito *carro= new Carrito();
-
-     //Varaibels de interfaz gr[afica
-    carro->capacidad = 10;
-    carro->duracionTotal = 5;
-
-
-    Almacen * almacenNuevo = new Almacen(carro, registro);
-
-    ArrayMachines *arraymachines = new ArrayMachines();
-
-    Receta * recetaGalletas = new Receta(5,5);
-
-    Main_Struct * mainStruct = new Main_Struct(almacenNuevo, arraymachines, recetaGalletas);
-
-
-    Ensambladora * nuevaEnsabladora = new Ensambladora();
-
-    mainStruct->ensambladora  = nuevaEnsabladora;
-    int varibleCEsar = 20;
-
-    mainStruct->arrayMachine->array[0]->min = varibleCEsar;
-    mainStruct->arrayMachine->array[0]->max = 30;
-    mainStruct->arrayMachine->array[0]->duracionSegudos = 20;
-    mainStruct->arrayMachine->array[0]->gramosProcesar = 20;
-
-    mainStruct->arrayMachine->array[1]->min = 15;
-    mainStruct->arrayMachine->array[1]->max = 40;
-    mainStruct->arrayMachine->array[1]->cantNow = 25;
-    mainStruct->arrayMachine->array[1]->duracionSegudos =10;
-    mainStruct->arrayMachine->array[1]->gramosProcesar = 5;
-
-    mainStruct->arrayMachine->array[2]->min = 25;
-    mainStruct->arrayMachine->array[2]->max = 50;
-    mainStruct->arrayMachine->array[2]->duracionSegudos = 5;
-    mainStruct->arrayMachine->array[2]->gramosProcesar = 10;
+    MainStruct * mainStruct = cargarDatos();
 
     QLabel * arrayLbMachines[6];
     arrayLbMachines[0] = this->ui->lbMachine1;
@@ -64,8 +29,76 @@ void MainWindow::on_btnIniciar_clicked(){
     arrayLbMachines[4] = this->ui->lbDatosMachine2;
     arrayLbMachines[5] = this->ui->lbDatosMachine3;
 
+    QLabel * arrayLbEnsambladora[4];
+    arrayLbEnsambladora[0] = this->ui->lbDatosBanda1;
+    arrayLbEnsambladora[1] = this->ui->lbDatosBanda2;
+    arrayLbEnsambladora[2] = this->ui->lbDatosEnsambladora;
+    arrayLbEnsambladora[3] = this->ui->lbDatosBandaHorno;
+
     mainThread = new thread_main();
-    mainThread->__init__(mainStruct, this->ui->panelMain,this->ui->lbCarro, arrayLbMachines, this->ui->lbCola);
+    mainThread->__init__(mainStruct, this->ui->factoryPanel,this->ui->lbCarro, arrayLbMachines, this->ui->lbCola,arrayLbEnsambladora);
     mainThread->start();
 
 }
+
+
+
+void MainWindow::on_btnGotoDatos_clicked(){
+     ui->contentPanel->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_btnGotoSimulacion_clicked(){
+    ui->contentPanel->setCurrentIndex(0);
+
+}
+
+MainStruct * MainWindow::cargarDatos(){
+
+    //Almacen
+    RegistroAlmacen * registro = new RegistroAlmacen();
+
+    Carrito *carro= new Carrito();
+    carro->capacidad = ui->txtCapacidadCar->text().toInt();
+    carro->duracionTotal = ui->txtDurationCar->text().toInt();
+
+    Almacen * almacenNuevo = new Almacen(carro, registro);
+
+    //Machines
+    ArrayMachines *arraymachines = new ArrayMachines();
+    arraymachines->array[0]->min = ui->txtMinMezcladora1->text().toInt();
+    arraymachines->array[0]->max = ui->txtMaxMecladora1->text().toInt();
+    arraymachines->array[0]->duracionSegudos = ui->txtDurationMecladora1->text().toInt();
+    arraymachines->array[0]->gramosProcesar =  ui->txtCantProcesarMezcladora1->text().toInt();
+
+    arraymachines->array[1]->min = ui->txtMinMezcladora1->text().toInt();
+    arraymachines->array[1]->max = ui->txtMaxMecladora1->text().toInt();
+    arraymachines->array[1]->duracionSegudos = ui->txtDurationMecladora1->text().toInt();
+    arraymachines->array[1]->gramosProcesar =  ui->txtCantProcesarMezcladora1->text().toInt();
+
+    arraymachines->array[2]->min = ui->txtMinChocolatera->text().toInt();
+    arraymachines->array[2]->max = ui->txtMaxChocolatera->text().toInt();
+    arraymachines->array[2]->duracionSegudos = ui->txtDurationChocolatera->text().toInt();
+    arraymachines->array[2]->gramosProcesar =  ui->txtCantProcesarChocolatera->text().toInt();
+
+    Receta * recetaCookies = new Receta(5,5);
+
+    Horno * horno = new Horno();
+
+    //Main Struct
+    MainStruct * mainStruct = new MainStruct(almacenNuevo, arraymachines,recetaCookies, horno);
+    Ensambladora * nuevaEnsabladora = new Ensambladora();
+
+    nuevaEnsabladora->bandas->array[0]->capacidad = 50;
+    nuevaEnsabladora->bandas->array[1]->capacidad = 50;
+    nuevaEnsabladora->cant = 10;
+    nuevaEnsabladora->duracionSegundos = 5;
+
+    mainStruct->ensambladora  = nuevaEnsabladora;
+}
+
+
+
+
+
+
